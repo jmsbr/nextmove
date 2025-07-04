f6808adc3a315e0763824c2a4a0f746955819308
pedroheike <75595898+pedroheike@users.noreply.github.com>
2025-07-03 22:29:39 -0300

    Add files via upload

---
+++ Teste/pedidos.js
@@ -0,0 +1,236 @@
+const allDashboardData = {
+    "Geral": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "APG -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "CAN -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "CTL -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "CTN -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "PON -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "TRD -GO": {
+        totalPending: 1,
+        totalDelivered: 7,
+        totalOverall: 8,
+        drivers: [
+            { name: "TAC CARRO - PEDRO HENRIQUE AMARAL", pending: 1, delivered: 7 },
+            { name: "Total Geral", pending: 381, delivered: 3244 }
+        ]
+    },
+    "GYN 02-GO": {
+        totalPending: 63,
+        totalDelivered: 436,
+        totalOverall: 499,
+        drivers: [
+            { name: "TAC CARRO - VINICIUS VIGNER DE ALMEIDA", pending: 5, delivered: 108 },
+            { name: "TAC CARRO - ISRAEL RODRIGUES DE SENA", pending: 0, delivered: 100 },
+            { name: "TAC CARRO - LUCAS MATOS DA SILVA", pending: 21, delivered: 97 },
+            { name: "TAC CARRO - SARA CRISTINE RIBEIRO DOS SANTOS", pending: 7, delivered: 73 },
+            { name: "MEI CARRO - WISLEY ALVES DA SILVA", pending: 26, delivered: 58 },
+            { name: "MEI CARRO - CLAUDIO MOREIRA CARVALHO", pending: 4, delivered: 0 }
+        ]
+    },
+    "GYN 03-GO": {
+        totalPending: 62,
+        totalDelivered: 1013,
+        totalOverall: 1075,
+        drivers: [
+            { name: "MEI CARRO - WESLEY DIVINO FRANCO", pending: 3, delivered: 134 },
+            { name: "MEI CARRO - RENATO REZENDE DE BARROS", pending: 6, delivered: 119 },
+            { name: "MEI CARRO - THIAGO ROSA VITURINO", pending: 1, delivered: 84 },
+            { name: "TAC CARRO -  WYVES RIOS MORAES", pending: 2, delivered: 72 },
+            { name: "MEI CARRO - LEANDRO MANOEL DA SILVA", pending: 1, delivered: 69 },
+            { name: "TAC CARRO - JEZIEL FIGUEIRA GOMES", pending: 0, delivered: 67 },
+            { name: "TAC CARRO - VINICIUS MAIONE SILVA CRUZ", pending: 1, delivered: 64 },
+            { name: "TAC CARRO - STEFANNY BORGES DOS SANTOS", pending: 2, delivered: 62 },
+            { name: "TAC CARRO - EDSON LUIS DE OLIVEIRA", pending: 3, delivered: 60 },
+            { name: "TAC CARRO - UEDSON MARQUES MOITINHO", pending: 1, delivered: 59 },
+            { name: "MEI CARRO - FABRICIO ALVES PACHECO", pending: 0, delivered: 47 },
+            { name: "MEI CARRO - DIEGO LIMA REGO", pending: 0, delivered: 43 },
+            { name: "MEI CARRO - EDUARDA MARIAH MARTINS XAVIER CAMELO", pending: 8, delivered: 42 },
+            { name: "TAC CARRO - WALDEMIR SANTANA DA COSTA", pending: 0, delivered: 35 },
+            { name: "TAC CARRO - LUCAS FERREIRA ALVES MENDONCA", pending: 0, delivered: 33 },
+            { name: "TAC CARRO - LAIS MACHADO DOS SANTOS SOUZA", pending: 0, delivered: 16 },
+            { name: "MEI CARRO - FABIO NASCIMENTO SILVA", pending: 0, delivered: 7 },
+            { name: "TAC CARRO - HUGO FERREIRA FARIA", pending: 34, delivered: 0 }
+        ]
+    },
+    "GYN 04-GO": {
+        totalPending: 80,
+        totalDelivered: 244,
+        totalOverall: 324,
+        drivers: [
+            { name: "TAC CARRO - GLEYBE RENATO ANDRADE", pending: 5, delivered: 107 },
+            { name: "TAC CARRO - CARLOS HELIABY NEVES VIEIRA", pending: 2, delivered: 89 },
+            { name: "MEI CARRO - PEDRO HENRIQUE PORFIRIO DE OLIVEIRA", pending: 45, delivered: 48 },
+            { name: "TAC CARRO - JORDANA MENDES DE SOUSA", pending: 28, delivered: 0 }
+        ]
+    },
+    "GYN 05-GO": {
+        totalPending: 27,
+        totalDelivered: 324,
+        totalOverall: 351,
+        drivers: [
+            { name: "TAC CARRO - DEUSDETE FERREIRA DA ROCHA", pending: 17, delivered: 132 },
+            { name: "TAC CARRO - MARCIONILIO MOREIRA BARBOSA", pending: 10, delivered: 92 },
+            { name: "TAC CARRO - FERNANDA BEZERRA DA SILVA", pending: 0, delivered: 70 },
+            { name: "TAC CARRO - LEORGINA MARIA DUTRA SILVA JACINTO", pending: 0, delivered: 30 }
+        ]
+    },
+    "GYN 06-GO": {
+        totalPending: 15,
+        totalDelivered: 691,
+        totalOverall: 706,
+        drivers: [
+            { name: "TAC CARRO - WAGNER DIAS JUNIOR", pending: 1, delivered: 144 },
+            { name: "TAC CARRO - ELDER RODRIGUES NETO", pending: 1, delivered: 133 },
+            { name: "MEI CARRO - WANDERSON BARROS DA SILVA", pending: 1, delivered: 91 },
+            { name: "MEI CARRO - LEONILDA DA SILVA BRAGA CORDOVIL", pending: 2, delivered: 90 },
+            { name: "TAC CARRO - PRISCILA GENOVESI BOCCHI", pending: 2, delivered: 85 },
+            { name: "MEI CARRO - JULIENE DAYSE VASCONCELOS DA COSTA", pending: 0, delivered: 75 },
+            { name: "TAC CARRO - AMANCIO MARIANO FIRMINO FILHO", pending: 1, delivered: 73 },
+            { name: "TAC CARRO - RAFAELA DOSTOIEVSKI", pending: 7, delivered: 0 }
+        ]
+    },
+    "GYN 07-GO": {
+        totalPending: 37,
+        totalDelivered: 521,
+        totalOverall: 558,
+        drivers: [
+            { name: "TAC CARRO - WASHINGTON FERREIRA", pending: 5, delivered: 90 },
+            { name: "TAC CARRO - WELLINGTON RIBEIRO DE LIMA", pending: 9, delivered: 75 },
+            { name: "MEI CARRO - MARCOS JUNIO BUENO DE OLIVEIRA", pending: 4, delivered: 66 },
+            { name: "TAC CARRO - ANNE CAROLINY ALVES DOS REIS", pending: 3, delivered: 58 },
+            { name: "TAC CARRO - PAULO HENRIQUE FERNANDES DA SILVA", pending: 1, delivered: 57 },
+            { name: "MEI CARRO - WELLIKY MOREIRA DE CARVALHO", pending: 5, delivered: 42 },
+            { name: "TAC CARRO - JOSUE PEDRO ALVES GOMES", pending: 0, delivered: 37 },
+            { name: "MEI CARRO - GEOVANE DOS SANTOS OLIVEIRA", pending: 8, delivered: 35 },
+            { name: "TAC CARRO - SIRLEY CASTILHO CABRAL DA CRUZ", pending: 2, delivered: 32 },
+            { name: "TAC CARRO - SILOMI MATIAS PEREIRA", pending: 0, delivered: 29 }
+        ]
+    },
+    "ITR -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "JTI -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "QUI -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "RVD -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "SEN-GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "ANP -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "GNA -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "IPR -GO": {
+        totalPending: 0,
+        totalDelivered: 5,
+        totalOverall: 5,
+        drivers: [
+            { name: "MEI CARRO - RAFAEL DAMASCENO SILVA RIBEIRO", pending: 0, delivered: 3 },
+            { name: "TAC CARRO - IONE CAETANO DA ROCHA", pending: 0, delivered: 2 }
+        ]
+    },
+    "PRG -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "SMA -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "SMB -GO": {
+        totalPending: 0,
+        totalDelivered: 0,
+        totalOverall: 0,
+        drivers: [
+        ]
+    },
+    "URC -GO": {
+        totalPending: 96,
+        totalDelivered: 3,
+        totalOverall: 99,
+        drivers: [
+            { name: "MEI CARRO - LUZIA FRANCISCO OZORIO SANTOS", pending: 0, delivered: 3 },
+            { name: "TAC CARRO - MARCELO COSTA SILVA", pending: 78, delivered: 0 },
+            { name: "TAC CARRO - PATRICIA MARA DE OLIVEIRA", pending: 12, delivered: 0 },
+            { name: "TAC CARRO - EMERSON CAMPOS PIMENTEL", pending: 2, delivered: 0 },
+            { name: "TAC CARRO - EVERLAM NOVAES DE CARVALHO", pending: 4, delivered: 0 }
+        ]
+    }
+};
